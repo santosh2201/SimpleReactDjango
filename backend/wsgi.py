94dd13414e04e2466b6325216e925e30ddfc79b2
Ubuntu <ubuntu@ip-172-31-8-251.us-east-2.compute.internal>
2018-01-28 19:22:03 +0000

    ssl commits for wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -6,7 +6,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/ubuntu/SimpleReactDjango/backend')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/ubuntu/.virtualenvs/SimpleReactDjango/lib/python2.7/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
